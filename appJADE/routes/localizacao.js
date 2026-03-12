e345b1a2d079e6aee461e77445b69a0548a2fe21
Erick Inacio Trajano <erickinaciot@gmail.com>
2026-03-12 11:44:37 -0300

    Versão com páginas Produtos, Sobre e Localização

--- appJADE/routes/localizacao.js
+++ appJADE/routes/localizacao.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jade' }); 
+  res.render('localizacao', { title: 'Localização' });
 });
 
 module.exports = router;
